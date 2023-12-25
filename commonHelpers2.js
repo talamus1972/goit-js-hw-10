5b3470ce8e55c73fc0b377f5e084672b9989be7f
talamus1972 <140251335+talamus1972@users.noreply.github.com>
2023-12-25 04:57:00 +0000

    Deploying to gh-pages from @ talamus1972/goit-js-hw-10@884e99e20adf73f9587c307b0f297b682e3023c5 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";import"./assets/vendor-651d7991.js";const o=document.querySelector("delay"),t=document.querySelector("button");console.log(o);console.log(t);
+import"./assets/modulepreload-polyfill-ec808ebb.js";import"./assets/vendor-651d7991.js";const e=document.getElementsByTagName("input")[0];document.querySelector("button");const t=document.getElementsByTagName("input")[1],o=document.getElementsByTagName("input")[2];console.log(e);console.log(t);console.log(o);
 //# sourceMappingURL=commonHelpers2.js.map
