3b713a326ef5e3aa5f5678ff5d57b82d67bc6f37
talamus1972 <talamus1972@gmail.com>
2023-12-26 11:34:24 +0200

    2222222

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,88 +1,85 @@
 // Описаний в документації
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
+import 'flatpickr/dist/flatpickr.min.css';
 
 // Описаний у документації
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
 // Додатковий імпорт стилів
-import "izitoast/dist/css/iziToast.min.css";
+import 'izitoast/dist/css/iziToast.min.css';
 
 let userSelectedDate;
-const input = document.querySelector("#datetime-picker");
-const button = document.querySelector("button");
+const input = document.querySelector('#datetime-picker');
+const button = document.querySelector('button');
 button.setAttribute('disabled', 'true');
-button.classList.add("start");
+button.classList.add('start');
 
 const dataDays = document.querySelector('span[data-days]');
 const dataHours = document.querySelector('span[data-hours]');
 const dataMinutes = document.querySelector('span[data-minutes]');
 const dataSeconds = document.querySelector('span[data-seconds]');
 
-
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-    onClose(selectedDates) {
-         console.log(selectedDates[0]);
-        if (selectedDates[0] < options.defaultDate) {
-            button.setAttribute('disabled', 'true');
-            // window.alert("Please choose a date in the future");
-        iziToast.show({
-            position: 'topRight',
-            messageColor: '#FFFFFF',
-            backgroundColor: '#EF4040',
-            messageSize: '16px',
-            messageLineHeight: '1.5',
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0] < options.defaultDate) {
+      button.setAttribute('disabled', 'true');
+
+      iziToast.show({
+        position: 'topRight',
+        messageColor: '#FFFFFF',
+        backgroundColor: '#EF4040',
+        messageSize: '16px',
+        messageLineHeight: '1.5',
         message: 'Please choose a date in the future',
       });
-        }
-        button.removeAttribute('disabled')
-        userSelectedDate = selectedDates[0];              
-    },
+    }
+    button.removeAttribute('disabled');
+    userSelectedDate = selectedDates[0];
+  },
 };
 
 flatpickr(input, options);
 
-button.addEventListener("click", () => {
-    button.setAttribute('disabled', 'true');
+button.addEventListener('click', () => {
+  button.setAttribute('disabled', 'true');
 
-    const addLeadingZero = (value) => {
-        return value.toString().padStart(2, '0');
+  const addLeadingZero = value => {
+    return value.toString().padStart(2, '0');
+  };
+  const interval = setInterval(() => {
+    const selectedDateTime = userSelectedDate;
+    const currentDateTime = Date.now();
+    const timerTime = selectedDateTime - currentDateTime;
+    const result = convertMs(timerTime);
+    dataDays.textContent = `${result.days}`;
+    dataHours.textContent = `${result.hours}`;
+    dataMinutes.textContent = `${result.minutes}`;
+    dataSeconds.textContent = `${result.seconds}`;
+
+    const formattedDays = addLeadingZero(result.days);
+    const formattedHours = addLeadingZero(result.hours);
+    const formattedMinutes = addLeadingZero(result.minutes);
+    const formattedSeconds = addLeadingZero(result.seconds);
+
+    dataDays.textContent = formattedDays;
+    dataHours.textContent = formattedHours;
+    dataMinutes.textContent = formattedMinutes;
+    dataSeconds.textContent = formattedSeconds;
+
+    const zeroTime =
+      result.days + result.hours + result.minutes + result.seconds;
+    if (zeroTime === 0) {
+      clearInterval(interval);
+
+      console.log('Interval cleared');
     }
-   const interval = setInterval(() => {
-        const selectedDateTime = userSelectedDate;
-        const currentDateTime = Date.now();
-        const timerTime = selectedDateTime - currentDateTime;
-        const result = convertMs(timerTime);
-dataDays.textContent = `${result.days}`
-dataHours.textContent = `${result.hours}`
-dataMinutes.textContent = `${result.minutes}`
-dataSeconds.textContent = `${result.seconds}`
-       
-const formattedDays = addLeadingZero(result.days);
-const formattedHours = addLeadingZero(result.hours);
-const formattedMinutes = addLeadingZero(result.minutes);
-const formattedSeconds = addLeadingZero(result.seconds);
-       
-        dataDays.textContent = formattedDays;
-        dataHours.textContent = formattedHours;
-        dataMinutes.textContent = formattedMinutes;
-        dataSeconds.textContent = formattedSeconds;
-
-
-     const zeroTime = result.days + result.hours + result.minutes + result.seconds;
-        if (zeroTime === 0) {
-            clearInterval(interval);
-
-             console.log('Interval cleared');
-        }
   }, 1000);
 });
-
-
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -102,8 +99,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-
-
-
-
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,13 +1,44 @@
 // Описаний у документації
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
 // Додатковий імпорт стилів
-import "izitoast/dist/css/iziToast.min.css";
+import 'izitoast/dist/css/iziToast.min.css';
 
-const input = document.getElementsByTagName("input")[0];
-const button = document.querySelector("button");
+const input = document.getElementsByTagName('input')[0];
+const button = document.querySelector('button');
 const fulfilled = document.getElementsByTagName('input')[1];
 const rejected = document.getElementsByTagName('input')[2];
 
-console.log(input);
-console.log(fulfilled);
-console.log(rejected);
+let delay = '';
+
+input.addEventListener('input', event => {
+  delay = event.target.value;
+  console.log(delay);
+});
+
+fulfilled.addEventListener('click', () => {
+  console.log('herkk');
+});
+
+rejected.addEventListener('click', () => {
+  console.log('herffffffffffffffffffffffffkk');
+});
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    button.addEventListener('click', event => {
+      if (fulfilled.checked) {
+        resolve(`✅ Fulfilled promise in ${delay}ms`);
+      } else if (rejected.checked) {
+        reject(`❌ Rejected promise in ${delay}ms`);
+      }
+    });
+  }, 1000);
+});
+
+promise
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.log(error);
+  });
